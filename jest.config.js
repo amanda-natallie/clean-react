a8f26fb99c608bd03372b166a2df7e15ca599164
Amanda Natallie <amanda.natallie.2@gmail.com>
2022-03-19 04:14:29 -0300

    refactor: fix script type error + lint

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  moduleDirectories: ['node_modules', 'src'],
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
-  coverageDirectory: 'coverage',
-  testEnvironment: 'node',
+  moduleDirectories: ["node_modules", "src"],
+  roots: ["src"],
+  collectCoverageFrom: ["src/**/*.{ts,tsx}"],
+  coverageDirectory: "coverage",
+  testEnvironment: "node",
   // converts the test file before testing, because jest doesnt support typescript
   transform: {
-    '.+\\.ts$': 'ts-jest'
+    ".+\\.ts$": "ts-jest"
   }
 }
